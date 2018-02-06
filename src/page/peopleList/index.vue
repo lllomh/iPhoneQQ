1211a20d201ba61d1217c57f917076c1a9350f38
lllomh <lllomh@qq.com>
2018-02-06 20:49:35 +0800

    加ul li

--- src/page/peopleList/index.vue
+++ src/page/peopleList/index.vue
@@ -24,7 +24,7 @@
                 <div class="content">
                     <ul>
                         <li>
-
+                        0
                         </li>
                     </ul>
                 </div>
