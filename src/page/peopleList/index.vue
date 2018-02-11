7ff8c8731f5d62cacc60c2cee1123d439fc0fc5b
lllomh <lllomh@qq.com>
2018-02-11 19:23:33 +0800

    文字xiugai

--- src/page/peopleList/index.vue
+++ src/page/peopleList/index.vue
@@ -24,7 +24,7 @@
                 <div class="content">
                     <ul>
                         <li>
-                          test
+                          test1
                         </li>
                     </ul>
                 </div>
